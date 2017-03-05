2fcd5be6437f01793134dc135a7f3efb2cffd4b9
cswain27 <swain.cody@gmail.com>
2017-03-05 15:51:23 -0800

    Seperate CSV Reader

---
+++ stephScrape.py
@@ -0,0 +1,25 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+import nba_py.player
+import nba_py.shotchart
+import csv 
+''' with open('curry.csv', 'w', newline='') as fp:
+	a = csv.writer(fp, delimiter=',')
+
+	
+
+	a.writerows(curryShots.shot_chart())
+'''
+
+# Ask user which player they would like to scrape and calculate
+# hothandedness for
+
+# refers to NBA player's id on stats.nba.com
+# can be found in url for player's page
+playerId = 201939
+
+curryShots = nba_py.shotchart.ShotChart(201939)
+
+curryShots.shot_chart().to_csv(path_or_buf='curryshots.csv')
+
